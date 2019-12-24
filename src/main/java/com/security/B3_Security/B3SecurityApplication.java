7a7d5df74e5db048a54aaa4c6534057500a8c75c
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-24 13:35:19 +0530

    spring security.

---
+++ src/main/java/com/security/B3_Security/B3SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.B3_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B3SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/B3_Security/B3SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.B3_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
